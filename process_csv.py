8895b881e756fc92cca69c39aa9db07b90f25268
Tholleti-Pavani <pavanitholleti682@gmail.com>
2026-02-12 22:57:03 +0530

    Processed CSV files and generated formatted output

--- process_csv.py
+++ process_csv.py
@@ -5,10 +5,16 @@ df1 = pd.read_csv("data/daily_sales_data_0.csv")
 df2 = pd.read_csv("data/daily_sales_data_1.csv")
 df3 = pd.read_csv("data/daily_sales_data_2.csv")
 
-# Keep only Pink Morsels
-df1 = df1[df1['product'] == "Pink Morsel"]
-df2 = df2[df2['product'] == "Pink Morsel"]
-df3 = df3[df3['product'] == "Pink Morsel"]
+# Normalize product names and keep only pink morsels
+df1 = df1[df1['product'].str.strip().str.lower() == "pink morsel"]
+df2 = df2[df2['product'].str.strip().str.lower() == "pink morsel"]
+df3 = df3[df3['product'].str.strip().str.lower() == "pink morsel"]
+
+# Remove $ sign from price and convert to float
+df1['price'] = df1['price'].replace(r'[\$,]', '', regex=True).astype(float)
+df2['price'] = df2['price'].replace(r'[\$,]', '', regex=True).astype(float)
+df3['price'] = df3['price'].replace(r'[\$,]', '', regex=True).astype(float)
+
 
 # Calculate Sales
 df1['Sales'] = df1['quantity'] * df1['price']
@@ -27,3 +33,4 @@ combined_df = pd.concat([df1, df2, df3])
 combined_df.to_csv("data/formatted_output.csv", index=False)
 
 print("CSV files processed successfully!")
+
